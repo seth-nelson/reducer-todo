1764cc2b53074a115866b90170f809ddb32189bf
Seth Nelson <seth.nelson@outlook.com>
2020-04-08 17:49:40 -0700

    got todoForm working sufficiently

--- todo/src/components/TodoForm.js
+++ todo/src/components/TodoForm.js
@@ -1,6 +1,47 @@
 import React from 'react';
 
+const TodoForm = ({dispatch}) => {
 
+    const [todo, setTodo] = useState('');
+
+    const submitForm = e => {
+        e.preventDefault();
+        dispatch({
+            type: 'ADD_TASK',
+            payload: item,
+        });
+        setItem('');
+    };
+
+    const clearCompleteTasks = e => {
+        e.preventDefault();
+        dispatch({
+            type: 'CLEAR_COMPLETE_TASK',
+        });
+    };
+
+    const handleChanges = e => {
+        setItem(e.target.value);
+    };
+
+
+    return (
+        <div>
+            <form onSubmit={submitForm}>
+                <div className='add-task-container'>
+                    <input 
+                        name='add-task'
+                        onChange={handleChanges} value={item} 
+                    />
+                    <button className='add-btn'>Add Task</button>
+                </div>
+                <div className='clr-complete-btn-container'>
+                    <button className='clr-completed-btn' onClick={clearCompleteTasks}>Clear Completed Tasks</button>
+                </div>
+            </form>
+        </div>
+    )
+}
 
 
 
--- todo/src/reducers/titleReducer.js
+++ todo/src/reducers/titleReducer.js
@@ -22,26 +22,21 @@ export const titleReducer = (state, action) => {
         case "ADD_TASK":
             return {
                 ...state,
+                item: action.payload,
                 completed: false,
                 id: (Math.random() * Math.random()).toString(9).substr(2, 9)
             };
         case "TOGGLE_COMPLETE_TASK":
-            return {
-                ...state,
-                state.map(task => {
+            return state.map(task => {
                     if (task.id === action.payload) {
                         return {...task, complete: !task.complete};
                     }
                 return task;
-                })
-            };
+                });
         case "CLEAR_COMPELTE_TASK":
-            return {
-                ...state,
-                state.filter(task => {
+            return state.filter(task => {
                     return !task.complete;
-                })
-            };
+                });
         default:
             return state;
     }
